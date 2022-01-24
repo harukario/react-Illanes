24e84ec304e34c2eccfc8a613ad33c489b4a35e8
Valentina Illanes <ivaleillanes@gmail.com>
2022-01-24 17:58:30 -0300

    primer-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import headerApp from './components/headerApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">     
+      <headerApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/logoHeader.js
@@ -0,0 +1,11 @@
+const logoHeader = () => {
+    return (
+      <img
+        src="https://1000marcas.net/wp-content/uploads/2020/02/logo-Google.png"
+        alt="logo"
+        width="50px"
+      />
+    );
+  };
+  
+  export default logoHeader;
